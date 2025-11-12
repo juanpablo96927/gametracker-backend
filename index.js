fe6108a0568436ca0495ae7b9f18fc679bdc51b3
Juan Pablo <juanpablo@Juans-MBP.lan>
2025-11-12 11:32:51 -0700

    Creacion del sistema de reseñas y automatizacion de puntuacion media

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors'); 
 
 const app = express();
 const port = process.env.PORT;
 
 // Middleware
+app.use(cors());
 app.use(express.json());
+
+// IMPORTAR TODAS LAS RUTAS
+const reviewRoutes = require('./routes/reviewRoutes');
+const gameRoutes = require('./routes/gameRoutes');
 
 // Connect to MongoDB Atlas
 const conectarDB = async () => {
@@ -20,26 +26,19 @@ console.error('Error de conexión:', error);
 process.exit(1);
 }
 };
-
 conectarDB();
 
-// 1. IMPORTAR TODAS LAS RUTAS
-const reviewRoutes = require('./routes/reviewRoutes');
-const gameRoutes = require('./routes/gameRoutes');
-
+// 2. CONECCION DE RUTAS
+app.use('/api/juegos/:gameId/reviews', reviewRoutes);
+app.use('/api/juegos', gameRoutes);
 
 // Rutas de prueba
 app.get('/', (req, res) => {
 res.send('Servidor GameTracker Backend Funcionando en localhost!');
 });
 
-
-// 2. CONECCION DE RUTAS
-app.use('/api/juegos/:gameId/reviews', reviewRoutes);
-app.use('/api/juegos', gameRoutes);
-
-
 // SERVIDOR LOCAL
 app.listen(port, () => {
 console.log(`Servidor corriendo en http://localhost:${port}`);
 });
+
--- models/Game.js
+++ models/Game.js
@@ -12,7 +12,6 @@ const gameSchema = new mongoose.Schema({
     completado: { type: Boolean, default: false },
     puntuacion: { type: Number, min: 0, max: 5, default: 0 },
     horasJugadas: { type: Number, default: 0 },
-    fechaCreacion: { type: Date, default: Date.now },
     reviews: [
         {
             // Especifica que el tipo es un ID de MongoDB
@@ -20,7 +19,21 @@ const gameSchema = new mongoose.Schema({
             // Especifica el nombre del modelo al que hace referencia
             ref: 'Review' 
         }
-    ]
+    ],
+    fechaCreacion: { 
+        type: Date,
+        default: Date.now 
+    }
+},
+// Inclusion de la puntiacion media en el JSON
+{
+    toJSON: { virtuals: true }, 
+    toObject: { virtuals: true }
+});
+
+// Nombramiento de la puntuacion media a traves del campo puntuacion
+gameSchema.virtual('puntuacionMedia').get(function() {
+    return this.puntuacion;
 });
 
 module.exports = mongoose.model('Game', gameSchema);
